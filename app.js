2b454af8ba0cb4f2a47ec41890ebfb5a29e70472
SoumenNath <soumennath120@gmail.com>
2025-07-24 21:04:39 -0400

    Fix broken health check

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...'
   res.send('ok')
 })
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
